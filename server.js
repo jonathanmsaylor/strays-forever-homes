fb3d4de0758ff11e76ec3f3a3db8dc6aaf07c0f4
Jonathan Saylor <mashsaylor@hotmail.com>
2019-03-18 18:54:16 -0700

    bug fix

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ var mongoose = require("mongoose");
 var bodyParser = require('body-parser');
 var app = express();
 
-app.use(express.static(__dirname+'/public/dist/public'));
+app.use(express.static(__dirname+'/public/dist'));
 
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect('mongodb://localhost:27017/cat_questions', {useNewUrlParser: true});
 
 require('./server/config/mongoose.js');
 
 var routes_setter = require('./server/config/routes.js');
 routes_setter(app);
 
-app.listen(process.env.PORT || 8000)
+app.listen(process.env.PORT || 8000);
